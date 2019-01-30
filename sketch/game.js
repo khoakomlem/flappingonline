d7f1cb36e0545045e2b0a5e1d38059557e17506f
Khoa Dau <visualpascalc@gmail.com>
2019-01-29 19:27:30 -0800

    1

--- sketch/game.js
+++ sketch/game.js
@@ -1,6 +1,5 @@
 var piperank=[];
 function preload(){
-<<<<<<< HEAD
 	bg=loadImage('sketch/image/bg.png');
 	me=new Bird();
 	skin=loadImage('sketch/image/bird.png');
@@ -8,9 +7,6 @@ function preload(){
 	pImage1=loadImage('sketch/image/pipe1.jpg');
 	pImage2=loadImage('sketch/image/pipe2.jpg');
 	deathimg=loadImage('sketch/image/skull.png');
-=======
-	// var img=loadImage('sketch/image/t.jpg');
->>>>>>> d27176f6de51e024ba930f96852cafb43e488b86
 }
 function setup(){
 	var canv = createCanvas(Width, Height);
